30dc915daf4cbf33a61cd87bcf306c421d65fdf4
Pedro Rennan <pedrorennan4444@gmail.com>
2025-12-13 00:24:37 -0300

    Melhoria na ordernacao por likes e abertura pra outros tipos de ordenacao

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 
 const contentDiv = document.querySelector(".content")
 const filterSelect = document.querySelector("#categoryFilter")
-const popularityFilter = document.querySelector("#likesFilter")
+const orderSelect = document.querySelector("#orderBy")
 const searchInput = document.querySelector("#searchInput")
 const dateFilterMin = document.querySelector("#dateFilterMin")
 const dateFilterMax = document.querySelector("#dateFilterMax")
 const clearFiltersBttn = document.querySelector("#clearFiltersBttn")
 
 const dados = [
-    {id: 1, titulo: "Campeonato de Stop", categoria: "Lazer", data:"2025-12-10", curtidas: 4, liked: false},
+    {id: 1, titulo: "Campeonato de Stop", categoria: "Lazer", data:"2025-12-10", curtidas: 4, liked: false}, 
     {id: 2, titulo: "Projetos de LSW", categoria: "Educação", data:"2025-12-18", curtidas: 1, liked: false},
     {id: 3, titulo: "Carnaval", categoria: "Lazer", data:"2026-02-13", curtidas: 2, liked: false},
     {id: 4, titulo: "Dia D para Vacinação", categoria: "Saúde", data:"2026-04-05", curtidas: 1, liked: false},
@@ -17,7 +17,7 @@ const dados = [
 
 const defaultFilters = [
     {type: "categoria", value: "todas"},
-    {type: "popularidade", value: false},
+    {type: "ordenacao", value: ""},
     {type: "titulo", value: ""},
     {type: "minDate", value: null},
     {type: "maxDate", value: null}
@@ -65,7 +65,7 @@ function createEventCard(event){
     
     //Função de curtir
     cardLikeBttn.addEventListener("click", () => {
-        if (!event.liked) {
+        if (!event.liked) { 
             event.curtidas++
             cardLikeCount.textContent = event.curtidas
             event.liked = true
@@ -131,6 +131,7 @@ function updateFilters(type, value){
 function clearFilters(){
     activeFilters = defaultFilters.map(f => ({...f}));
     filterSelect.value = "todas";
+    orderSelect.value = "";
     popularityFilter.checked = false;
     searchInput.value = "";
     dateFilterMin.value = "";
@@ -141,6 +142,19 @@ function clearFilters(){
 function filterByCategory(event){
     const categoryFilter = activeFilters.find(f => f.type === "categoria").value;
     return categoryFilter === "todas" || event.categoria.toLowerCase() === categoryFilter;
+}
+
+function orderBy(event) {
+    const orderBy = activeFilters.find(f => f.type === "ordenacao").value;
+    if (!orderBy) {
+        return event
+    }
+    const ordered = [...event]
+    switch (orderBy) {
+        case "likes":
+            ordered.sort((a, b) => b.curtidas - a.curtidas);
+    }
+    return ordered
 }
 
 function filterBySearch(event){
@@ -163,22 +177,15 @@ function filterByMaxDate(event){
 }
 
 function filterEvents(){
-    const filteredEvents = dados.filter(event => {
+    let filteredEvents = dados.filter(event => {
         const matchesCategory = filterByCategory(event);
         const matchesSearch = filterBySearch(event);
         const matchesDate = filterByMinDate(event) && filterByMaxDate(event);
         return matchesCategory && matchesSearch && matchesDate;
     })
 
-    //Ordenação por popularidade
-    const order = activeFilters.find(f => f.type === "popularidade")?.value;
-    console.log(order);
-    let filteredEventsOrdened = filteredEvents;
-    if (order) {
-        filteredEventsOrdened = [...filteredEvents].sort((a, b) => b.curtidas - a.curtidas);
-    }
-    
-    renderEvents(filteredEventsOrdened)
+    filteredEvents = orderBy(filteredEvents)
+    renderEvents(filteredEvents)
 }
 
 
@@ -189,8 +196,8 @@ function initialize(){
     populateCategoryFilter();
 
     filterSelect.addEventListener("change", event => updateFilters("categoria", event.target.value));
-    popularityFilter.checked = false;
-    popularityFilter.addEventListener("change", e => updateFilters("popularidade", e.target.checked));
+    orderSelect.value = "";
+    orderSelect.addEventListener("change", event => updateFilters("ordenacao", event.target.value));
     searchInput.addEventListener("input", event => updateFilters("titulo", event.target.value));
     dateFilterMin.addEventListener("change", event => updateFilters("minDate", event.target.value));
     dateFilterMax.addEventListener("change", event => updateFilters("maxDate", event.target.value));
