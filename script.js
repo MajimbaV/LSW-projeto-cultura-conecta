04d46ff9a16d20596921d0873d15958cace20c95
Pedro Rennan <pedrorennan4444@gmail.com>
2025-12-12 23:34:57 -0300

    Funcionalidade de ordenacao por likes implementada

--- script.js
+++ script.js
@@ -2,20 +2,22 @@
 
 const contentDiv = document.querySelector(".content")
 const filterSelect = document.querySelector("#categoryFilter")
+const popularityFilter = document.querySelector("#likesFilter")
 const searchInput = document.querySelector("#searchInput")
 const dateFilterMin = document.querySelector("#dateFilterMin")
 const dateFilterMax = document.querySelector("#dateFilterMax")
 const clearFiltersBttn = document.querySelector("#clearFiltersBttn")
 
 const dados = [
-    {id: 1, titulo: "Campeonato de Stop", categoria: "Lazer", data:"2025-12-10", curtidas: 0},
-    {id: 2, titulo: "Projetos de LSW", categoria: "Educação", data:"2025-12-18", curtidas: 0},
-    {id: 3, titulo: "Carnaval", categoria: "Lazer", data:"2026-02-13", curtidas: 0},
-    {id: 4, titulo: "Dia D para Vacinação", categoria: "Saúde", data:"2026-04-05", curtidas: 0},
+    {id: 1, titulo: "Campeonato de Stop", categoria: "Lazer", data:"2025-12-10", curtidas: 4, liked: false},
+    {id: 2, titulo: "Projetos de LSW", categoria: "Educação", data:"2025-12-18", curtidas: 1, liked: false},
+    {id: 3, titulo: "Carnaval", categoria: "Lazer", data:"2026-02-13", curtidas: 2, liked: false},
+    {id: 4, titulo: "Dia D para Vacinação", categoria: "Saúde", data:"2026-04-05", curtidas: 1, liked: false},
 ]
 
 const defaultFilters = [
     {type: "categoria", value: "todas"},
+    {type: "popularidade", value: false},
     {type: "titulo", value: ""},
     {type: "minDate", value: null},
     {type: "maxDate", value: null}
@@ -62,12 +64,12 @@ function createEventCard(event){
     cardLikeBttn.textContent = "Curtir"
     
     //Função de curtir
-    let likedThis = 0
     cardLikeBttn.addEventListener("click", () => {
-        if (likedThis == 0) {
+        if (!event.liked) {
             event.curtidas++
             cardLikeCount.textContent = event.curtidas
-            likedThis = 1
+            event.liked = true
+            filterEvents()
         }
     })
 
@@ -129,6 +131,7 @@ function updateFilters(type, value){
 function clearFilters(){
     activeFilters = defaultFilters.map(f => ({...f}));
     filterSelect.value = "todas";
+    popularityFilter.checked = false;
     searchInput.value = "";
     dateFilterMin.value = "";
     dateFilterMax.value = "";
@@ -166,7 +169,16 @@ function filterEvents(){
         const matchesDate = filterByMinDate(event) && filterByMaxDate(event);
         return matchesCategory && matchesSearch && matchesDate;
     })
-    renderEvents(filteredEvents)
+
+    //Ordenação por popularidade
+    const order = activeFilters.find(f => f.type === "popularidade")?.value;
+    console.log(order);
+    let filteredEventsOrdened = filteredEvents;
+    if (order) {
+        filteredEventsOrdened = [...filteredEvents].sort((a, b) => b.curtidas - a.curtidas);
+    }
+    
+    renderEvents(filteredEventsOrdened)
 }
 
 
@@ -177,6 +189,8 @@ function initialize(){
     populateCategoryFilter();
 
     filterSelect.addEventListener("change", event => updateFilters("categoria", event.target.value));
+    popularityFilter.checked = false;
+    popularityFilter.addEventListener("change", e => updateFilters("popularidade", e.target.checked));
     searchInput.addEventListener("input", event => updateFilters("titulo", event.target.value));
     dateFilterMin.addEventListener("change", event => updateFilters("minDate", event.target.value));
     dateFilterMax.addEventListener("change", event => updateFilters("maxDate", event.target.value));
